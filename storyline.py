a811e0e06f87ca777de02275e1026f1f4c3c4fbc
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2023-04-25 10:57:36 -0400

    shop

---
+++ storyline.py
@@ -0,0 +1,5 @@
+def shop():
+    dialogue = int(input('Give me your wallet or get the heck out.'))
+    print("1: Ask for drink, 2: Buy something else,")
+    if dialogue = 'Gimme a drink, Bartender':
+        print("It's like, bad for you and stuff.")

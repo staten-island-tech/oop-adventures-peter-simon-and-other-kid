5dc2a6ade7b3205814678f9921ab91b0a4c09754
simon-saffayeh <simonsaffayeh@gmail.com>
2023-04-25 10:57:12 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -6,39 +6,38 @@ class weapon:
         self.hit = hit
         self.name = name
     
-class armor:
-    def __init__(self,absorb,name):
-        self.absorb = absorb
-        self.name = name
     
 class location:
     def __init__(self,monster):
         self.monster = monster
 
 class monster:
-    def __init__(self,hp,power,hit,absorb,level,name):
+    def __init__(self,hp,power,hit,level,name):
         self.hp = hp
         self.power = power
         self.hit = hit
         self.absorb = absorb
         self.name = name
     
-    def attack()
-
+class spell:
+    def __init__(self,damage,stun,dark):
+        self.damage = damage
+        self.stun = stun
+        self.dark = dark
 
 # Declare monsters
-imp = monster(50,10,0.5,0,1,"imp")
-troll = monster(50,15,0.7,1,1,"troll")
-wolf = monster(80,25,0.6,1,2,"wolf")
-bone = monster(120,30,0.8,3,2,"bone")
-lich = monster(150,50,0.8,5,3,"lich")
-wizard = monster(150,44,0.9,5,3,"wizard")
-worm = monster(200,30,0.9,5,3,"worm")
-medusa = monster(200,35,0.9,3,3,"medusa")
-kary = monster(300,50,0.85,7,4,"kary")
-kraken = monster(400,60,0.8,8,4,"kraken")
-tiamat = monster(420,40,0.95,10,4,"tiamat")
-chaos = monster(600,80,0.9,12,5,"CHAOS")
+imp = monster(50,10,0.5,1,"imp")
+troll = monster(50,15,0.7,1,"troll")
+wolf = monster(80,25,0.6,2,"wolf")
+bone = monster(120,30,0.8,2,"bone")
+lich = monster(150,50,0.8,3,"lich")
+wizard = monster(150,44,0.9,3,"wizard")
+worm = monster(200,30,0.9,3,"worm")
+medusa = monster(200,35,0.9,3,"medusa")
+kary = monster(300,50,0.85,4,"kary")
+kraken = monster(400,60,0.8,4,"kraken")
+tiamat = monster(420,40,0.95,4,"tiamat")
+chaos = monster(600,80,0.9,5,"CHAOS")
 
 # Declare weapons
 wooden = weapon(30,0.7,"Wooden Sword",1)

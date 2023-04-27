af8bed19e36c352e3f7cb9818c8a1129d404d1af
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2023-04-27 11:02:11 -0400

    goods

--- storyline.py
+++ storyline.py
@@ -13,43 +13,42 @@ def shop():
             print('You have Dr. Pepper. DRINK!')
         else:
             print('No fencesitting! DRINK!')
-        mentosChoice = input('SHOPKEEPER: Would you like Mentos? 1: Yes, 2: No')
+        mentosChoice = input('SHOPKEEP: Would you like Mentos? 1: Yes, 2: No')
         if mentosChoice = 1:
             print('The soda (along with the can) explodes in your face and amuses the shopkeeper. Oops.')
-            if drink_choice = 1:
+            if drink_choice == 1:
                 change_hp(-10)
-            if drink_choice = 2:
+            if drink_choice == 2:
                 change_hp(-45)
-            if drink_choice = 3:
+            if drink_choice == 3:
                 change_hp(-20)
         elif mentosChoice = 2:
             print('You drink your soda. Best drink ever.')
             if drink_choice = 1:
-                print('Coke is VERY good. Refreshing.')
+                print('You gain 10 HP. Coke is VERY good.')
                 change_hp(20)
             if drink_choice = 2:
-                print('Pepsi SUCKS!')
+                print('You gain like 1 HP.')
                 change_hp(1)
-            if drink_choice = 3:
-                print('Dr Pepper is alright.')
-                change_hp(10)
         else:
             print('Stop fencesitting.')
     if dialogue == 2:
-        print('SHOPKEEPER: Would you like weapons or armor? 1: Get weapons, 2: Get armor')
+        print('SHOPKEEP: Would you like weapons or armor? 1: Get weapons, 2: Get armor')
         goodsChoice = int(input('SHOPKEEP: What are you waiting for? 1: Get weapons, 2: Get armor'))
         if goodsChoice = 1:
-            print('SHOPKEEPER: We have swords.')
+            print('We have swords.')
             weaponChoice = int(input('1: Iron Sword, cost 50 coins. 2: Silver Sword, cost 100 coins. 3: Titanium Sword, cost 200 coins. 4: Gold Sword, cost 350 coins. 5: Diamond Sword, cost 500 coins.'))
-                if weaponChoice = 1:
-                    if gold >= 50:
-                        equipChoice = int(input('You now have Iron Sword. Equip sword? 1: Yes, 2: No'))
-                        if equipChoice = 1:
-                            equip(Iron)
-                        if equipChoice = 2:
-                            print('Nothing happens.')
-                        else:
-                            print('Do something.')
+            if weaponChoice = 1:
+                if gold >= 50:
+                    print('You now have Iron Sword.')
+                elif gold < 50: 
+                    equipChoice = int(input('You now have Iron Sword. Equip sword? 1: Yes, 2: No'))
+                    if equipChoice = 1:
+                        equip(Iron)
+                    if equipChoice = 2:
+                        print('Nothing happens.')
+                    else:
+                        print('Do something.')
                     elif gold < 50: 
                         print('SHOPKEEPER: Get a job. Denied!')
                 if weaponChoice = 2:
@@ -74,3 +73,5 @@ def shop():
                         print("SHOPKEEPER: Your body pillow is sad now.")
         if goodsChoice = 2:
             print()
+
+                

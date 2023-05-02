3d649b144a54a6d47f45c889a938ce4d10d0e341
simon-saffayeh <simonsaffayeh@gmail.com>
2023-05-02 10:44:21 -0400

    t

--- actions.py
+++ actions.py
@@ -1,11 +1,12 @@
-name = input("Hello Adventure! What is your name? ")
+name = input("Hello Adventurer! What is your name? ")
 hp = 100 
 hp_t = 100
 exp = 0 
 gold = 100
 
 
-def change_hp(HP):
+def change_hp(hp,HP,hp_t):
+    print(hp)
     if HP + hp > hp_t:
         HP = (HP + hp) - hp_t
         print("You have reached your maximum hp! ")
@@ -16,13 +17,14 @@ def change_hp(HP):
         print(f"Your hp was decreased by {abs(HP)} ")
     print(f"Your current hp is {hp}")
 
-def change_exp(EXP):
+change_hp(50)
+def change_exp(exp, EXP):
     exp += EXP
-    if EXP > 0:
+    if EXP >= 0:
         print(f"Your exp increased by {abs(EXP)}")
     print(f"Your new exp is {exp} ")
 
-def change_gold(GOLD):
+def change_gold(gold, GOLD):
     if gold - GOLD < 0:
         print("You do not have enough gold for this item")
     else:

8905c6df1e8393f8ca8d6214822b659b723a4d5c
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2023-05-02 10:55:02 -0400

    merge again

--- storyline.py
+++ storyline.py
@@ -1,4 +1,5 @@
 from classes import *
+from actions import *
 
 def shop():
     dialogue = int(input('Give me your wallet or get the heck out. 1: Ask for drink, 2: Buy something else, 3: Leave'))
@@ -20,22 +21,22 @@ def shop():
         if mentosChoice == 1:
             print('The soda (along with the can) explodes in your face and amuses the shopkeeper. Oops.')
             if drink_choice == 1:
-                change_hp(-10)
+                change_hp(hp,-10,hp_t)
             if drink_choice == 2:
-                change_hp(-45)
+                change_hp(hp,-45,hp_t)
             if drink_choice == 3:
-                change_hp(-20)
+                change_hp(hp,-20,hp_t)
         elif mentosChoice == 2:
             print('You drink your soda. Best drink ever.')
             if drink_choice == 1:
                 print('You gain 20 HP. Coke is VERY good.')
-                change_hp(20)
+                change_hp(hp,20,hp_t)
             if drink_choice == 2:
                 print('You gain like 1 HP.')
-                change_hp(1)
+                change_hp(hp,1,hp_t)
             if drink_choice == 3:
                 print('You gain 10 HP. Pretty good.')
-                change_hp(10)
+                change_hp(hp,10,hp_t)
         else:
             print('Stop fencesitting.')
     if dialogue == 2:
@@ -100,19 +101,34 @@ def shop():
                 elif gold < 500:
                     print("SHOPKEEPER: Your body pillow is sad now.")
         if goodsChoice == 2:
-            print()
+            armor_choice = input(int("SHOPKEEPER: Are you going commando? 1: Buy leather armor, 2: Buy iron armor, 3: BUy silver armor, 4: Buy titanium armor, 5: Buy gold armor, 6: Buy diamond armor"))
+            if armor_choice == 1:
+                if gold 
+
 
 shop()
 
 def impDialogue():
-    print("Order on the Domino's app and earn points towards free pizza!")
+    print("IMP: Order on the Domino's app and earn points towards free pizza!")
     pizzaChoice = int(input("Order on the Domino's App? 1: Yes, 2: No, 3: Act stupid"))
     if pizzaChoice == 1:
         eatPizza = int(input('The imp hands you his pizza. Eat some? 1: Yes, 2: No, 3: Shove it down his throat')) # If I had a nickel for every time I ate a whole pizza pie in this building, I'd have two (soon three) nickels.
         if eatPizza == 1:
-            print('There are expired booges on it and you lose 50 HP.')
-            change_hp(-50)
+            print('There are expired boogers on it and you lose 50 HP.')
+            change_hp(hp,-50,hp_t)
         if eatPizza == 2:
             print('You reject the offer. Nothing happens.')
         if eatPizza == 3:
-            print('')
+            print('The imp chokes on an anchovy and loses half HP. Your loss, bucko.')
+    if pizzaChoice == 2:
+        print('Nothing happens.')
+    if pizzaChoice == 3:
+        lunatic = int(input('You act like a deranged lunatic, causing the imp to back away. 1: Chase him, 2: Stay in place, 3: Yell stupid movie quotes'))
+        if lunatic == 1:
+            print('You rob the imp of his hard-earned gold. Way to go.')
+            change_gold(30)
+        if lunatic == 2:
+            print('No one wins. No one loses. The fight ends.')
+        if lunatic == 3:
+            print("You scream some lines from those corny kids' movies and make a complete fool of yourself. You lose 1 HP.")
+            change_hp(hp,-1,hp_t)

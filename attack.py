ac2b36b15a70f2a1abaec314c8c0e4e0a2bffddb
simon-saffayeh <simonsaffayeh@gmail.com>
2023-05-04 10:29:17 -0400

    Revert "Merge pull request #9 from staten-island-tech/Attack"
    This reverts commit bca0e08f14f3c34e657ceebbf60c80ce86aab2d2, reversing
    changes made to b6e66d31d3f0d0683ab9b3257e1468222dbd0909.

--- attack.py
+++ attack.py
@@ -8,19 +8,16 @@ def bartender_attack():
     while hp > 0 and bartender.hp > 0:
         print(f"Your HP: {hp}")
         print(f"Bartenders HP: {bartender.hp}")
-        move = False
-        while move == False:
-            choice = int(input("1. Attack 2. Magic"))
-            if choice == 1:
-                print("You attack the Bartender")
-                bartender.attacked()
-                move = True
-            if choice == 2:
-                if not magic:
-                    print("You havent learned any magic yet")
-        if(bartender.hp > 0):
-            bartender.attack(hp)
+        choice = int(input("1. Attack 2. Magic"))
 
+        if choice == 1:
+            print("You attack the Bartender")
+            bartender.attack()
+        if choice == 2:
+            if not magic:
+                print("You havent learned any magic yet")
+            else:
+                print(magic)
 
 
 

ac9396d7528ee4afe8075d746f176be7c41c1696
simon-saffayeh <simonsaffayeh@gmail.com>
2023-05-05 10:58:35 -0400

    m

--- attack.py
+++ attack.py
@@ -1,6 +1,5 @@
 from classes import *
 from storyline import *
-from actions import *
 import random
 
 
@@ -19,11 +18,38 @@ def bartender_attack():
                 if not magic:
                     print("You havent learned any magic yet")
         if(bartender.hp > 0):
+            print("The bartender takes out the colazuka and blasts you")
             bartender.attack(hp)
 
 
 
-
-
-
-bartender_attack()
+def troll_attack():
+   while hp > 0 and troll.hp > 0:
+        print(f"Your HP: {hp}")
+        print(f"Troll HP: {troll.hp}")
+        move = False
+        while move == False:
+            choice = int(input("1. Attack 2. Magic"))
+            if choice == 1:
+                print("You attack the Troll")
+                troll.attacked()
+                move = True
+            if choice == 2:
+                if not magic:
+                    print("You havent learned any magic yet")
+                else:
+                    print("What magic would you like to use?")
+                    if magic_num > 0:
+                        print("1. Fire",end=" ")
+                    if magic_num > 1:
+                        print("2. Dark",end=" ")
+                    if magic_num > 2:
+                        print("3. Heal",end=" ")     
+                    if magic_num > 3:
+                        print("4. Stun",end=" ")     
+                    if magic_num > 4:
+                        print("5. DOOM",end="")     
+                                                                                             
+        if(troll.hp > 0):
+            print("The Troll pulls out his pizza box and whacks you")
+            troll.attack(hp)

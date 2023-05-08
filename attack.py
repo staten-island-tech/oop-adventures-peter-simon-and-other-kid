61a3b0848efc113e129bc310ce1f6e68f35b3837
simon-saffayeh <simonsaffayeh@gmail.com>
2023-05-08 10:58:18 -0400

    Update attack.py

--- attack.py
+++ attack.py
@@ -1,6 +1,5 @@
 from classes import *
 from storyline import *
-from actions import *
 import random
 
 
@@ -19,11 +18,44 @@ def bartender_attack():
                 if not magic:
                     print("You havent learned any magic yet")
         if(bartender.hp > 0):
+            print("The bartender takes out the colazuka and blasts you")
             bartender.attack(hp)
 
 
 
-
-
-
-bartender_attack()
+def troll_attack():
+   while hp > 0 and troll.hp > 0:
+        print(f"Your HP: {hp}")
+        print(f"Troll HP: {troll.hp}")
+        move = False
+        while move == False:
+            choice = int(input("1. Attack 2. Magic"))
+            if choice == 1:
+                print("You attack the Troll")
+                troll.attacked()
+                move = True
+            if choice == 2:
+                if magic_num < 1:
+                    print("You havent learned any magic yet")
+                else:
+                    if magic_num > 0:
+                        print("1. Fire",end=" ")
+                    if magic_num > 1:
+                        print("2. Dark",end=" ")
+                    if magic_num > 2:
+                        print("3. Heal",end="")     
+                    if magic_num > 3:
+                        print("4. Stun",end=" ")     
+                    if magic_num > 4:
+                        print("5. DOOM",end="")    
+                    magic_choice = int(input(("What magic would you like to use?")))
+                if magic_choice <= magic_num and magic_choice > 0:
+                    if magic_choice == 3:
+                        heal = hp += (20 + random.randint(1,10))
+                        print(f"You gained {heal} hp")
+                    else: 
+                        troll.magic(magic_num)
+                                                                                             
+        if(troll.hp > 0 and check_stun() == False):
+            print("The Troll pulls out his pizza box and whacks you")
+            troll.attack(hp)
